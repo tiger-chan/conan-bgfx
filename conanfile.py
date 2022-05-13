007d723993636b4496bd0dcf1d933cbb6935220d
Anthony <anthony.r.young@gmail.com>
2022-05-12 21:32:13 -0600

    Modernize conan methods used

--- conanfile.py
+++ conanfile.py
@@ -1,41 +1,57 @@
-from   conans       import ConanFile, CMake
-from   distutils.dir_util import copy_tree
+from conans import ConanFile, tools
+from conan.tools.cmake import CMakeToolchain, CMake, CMakeDeps
+
 
 class BgfxConan(ConanFile):
     name            = "bgfx"
-    version         = "7816-3"
+    version         = "1.115.8266-06b9950"
+    """Tag from bgfx.cmake repo"""
     description     = "Conan package for bgfx."
     url             = "https://github.com/bkaradzic/bgfx"
     license         = "BSD"
     settings        = "arch", "build_type", "compiler", "os"
-    generators      = "cmake"
     options         = {
-            "shared": [True, False],
-            "multithreaded": [True, False]
-            }
+        "shared": [True, False],
+        "multithreaded": [True, False],
+        "build_tools": [True, False]
+    }
     default_options = {
-            "shared": False,
-            "multithreaded": True
-            }
+        "shared": False,
+        "multithreaded": True,
+        "build_tools": False
+    }
 
     def source(self):
-        self.run("git clone git@github.com:firefalcom/bgfx.cmake.git")
-        self.run("cd bgfx.cmake && git checkout v%s" % self.version)
-        copy_tree("bgfx.cmake", ".")
-        self.run("git submodule update --init --recursive")
+        git = tools.Git(folder=".")
+        version = "v" + self.version
+        try:
+            git.checkout(version)
+        except:
+            git.clone("https://github.com/bkaradzic/bgfx.cmake.git", branch=version)
+
+        git.run("submodule update --init --recursive")
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generate()
+
+        deps = CMakeDeps(self)
+        deps.generate()
 
     def build(self):
-        cmake          = CMake(self)
-        options = {
+        cmake = CMake(self)
+        variables = {
             "BUILD_SHARED_LIBS": self.options.shared,
             "BGFX_CONFIG_MULTITHREADED": self.options.multithreaded,
             "BGFX_BUILD_EXAMPLES": False,
-            "BGFX_BUILD_TOOLS": False,
+            "BGFX_BUILD_TOOLS": self.options.build_tools,
             "BGFX_OPENGL_VERSION": 33
-            }
+        }
 
-        cmake.configure(None, options)
+        cmake.configure(variables=variables)
         cmake.build()
+        if self.options.build_tools:
+            cmake.build(target="shaderc")
 
     def collect_headers(self, include_folder):
         self.copy("*.h"  , dst="include", src=include_folder)
@@ -50,12 +66,13 @@ class BgfxConan(ConanFile):
         self.copy("*.so" , dst="lib", keep_path=False)
         self.copy("*.lib", dst="lib", keep_path=False)
         self.copy("*.dll", dst="bin", keep_path=False)
-        self.copy("shaderc", dst="bin")
         self.copy("*.exe", dst="bin", keep_path=False)
+        self.copy("shaderc", dst="bin")
 
     def package_info(self):
         self.cpp_info.libs = ["bgfx", "bimg", "bx"]
         self.cpp_info.libs.extend(["astc-codec", "astc", "edtaa3", "etc1", "etc2", "iqa", "squish", "pvrtc", "tinyexr"])
+		
         if self.settings.os != "Switch":
             self.cpp_info.libs.extend(["nvtt"])
         if self.settings.os == "Macos":
